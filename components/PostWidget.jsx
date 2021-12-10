d0aff2953aad8860c72f0663fdf83a8a1831e356
LAPTOP-QIDO0NP0\Howard Mui <mhw19972013@gmail.com>
2021-12-10 13:37:46 +0800

    Discover the new npm of moment for the Date and coding with the Related Posts components(PostWidget.jsx)

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import moment from "moment";
+import Link from "next/link";
+
+// For fetch most recent post
+// --> 要起多個function (GQL) 去拎到果啲data
 
 const PostWidget = () => {
+  const [relatedPosts, setRelatedPosts] = useState;
+
   return <div>PostWidget</div>;
 };
 
--- services/index.js
+++ services/index.js
@@ -38,3 +38,24 @@ export const getPosts = async () => {
   //   console.log("This is results" + results);
   return results.postsConnection.edges;
 };
+
+export const getRecentPosts = async () => {
+  const query = gql`
+    query getPostDetails() {
+        posts(
+            orderBy: createAt_ASC
+            last: 3
+        ) {
+            title
+            featuredImage {
+                url
+            }
+            createdAt
+            slug
+        }
+    }
+    `;
+
+  const results = await request(hraphqlAPI, query);
+  return results.postsConnection.edges;
+};
