a6e6bc55bd17a2e9a4e19f2ebd0e4e49d407c8b6
LAPTOP-QIDO0NP0\Howard Mui <mhw19972013@gmail.com>
2021-12-12 10:57:35 +0800

    20211212 Wanna add the PostWidget component but occur error.  May be the problem occur from the graphQL API playground.

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,14 +1,111 @@
+// import React, { useState, useEffect } from "react";
+// import moment from "moment";
+// import Link from "next/link";
+// import { getRecentPosts, getSimilarPosts } from "../services";
+
+// //呢個page係要做recent posts(Home Page) and related posts(禁入個posts 之後)
+
+// // For fetch most recent post
+// // --> 要起多個function (GQL) 去拎到果啲data
+
+// const PostWidget = ({ categories, slug }) => {
+//   const [relatedPosts, setRelatedPosts] = useState([]);
+
+//   console.log(categories);
+//   console.log(slug);
+
+//   //要check user 係係home page 定 post pages 入邊？ --> 可以用slug去區分係邊一樣
+//   // slug 係講緊url 入邊嘅query?
+
+//   useEffect(() => {
+//     if (slug) {
+//       getSimilarPosts(categories, slug).then((result) =>
+//         setRelatedPosts(result)
+//       );
+//     } else {
+//       getRecentPosts().then((result) => setRelatedPosts(result));
+//     }
+//   }, [slug]);
+
+//   // console.log(relatedPosts);
+
+//   return (
+//     <div className="bg-white shadow-lg rounded-lg p-8 mb-8">
+//       <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+//         {slug ? "Related Post" : "Recent Posts"}
+//       </h3>
+//       {relatedPosts.map((post) => (
+//         <div key={post.title} className="flex items-center w-full mb-4">
+//           <div className="2-16 flex-none">
+//             <img
+//               alt={post.title}
+//               height="60px"
+//               width="60px"
+//               className="align-middle rounded-full"
+//               src={post.featuredImage.url}
+//             ></img>
+//           </div>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default PostWidget;
+
 import React, { useState, useEffect } from "react";
+import Image from "next/image";
 import moment from "moment";
 import Link from "next/link";
 
-// For fetch most recent post
-// --> 要起多個function (GQL) 去拎到果啲data
+// import { grpahCMSImageLoader } from "../util";
+import { getSimilarPosts, getRecentPosts } from "../services";
 
-const PostWidget = () => {
-  const [relatedPosts, setRelatedPosts] = useState;
+const PostWidget = ({ categories, slug }) => {
+  const [relatedPosts, setRelatedPosts] = useState([]);
 
-  return <div>PostWidget</div>;
+  useEffect(() => {
+    if (slug) {
+      getSimilarPosts(categories, slug).then((result) => {
+        setRelatedPosts(result);
+      });
+    } else {
+      getRecentPosts().then((result) => {
+        setRelatedPosts(result);
+      });
+    }
+  }, [slug]);
+
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+        {slug ? "Related Posts" : "Recent Posts"}
+      </h3>
+      {relatedPosts.map((post, index) => (
+        <div key={index} className="flex items-center w-full mb-4">
+          <div className="w-16 flex-none">
+            <Image
+              // loader={grpahCMSImageLoader}
+              alt={post.title}
+              height="60px"
+              width="60px"
+              unoptimized
+              className="align-middle rounded-full"
+              src={post.featuredImage.url}
+            />
+          </div>
+          <div className="flex-grow ml-4">
+            <p className="text-gray-500 font-xs">
+              {moment(post.createdAt).format("MMM DD, YYYY")}
+            </p>
+            <Link href={`/post/${post.slug}`} className="text-md" key={index}>
+              {post.title}
+            </Link>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default PostWidget;
--- services/index.js
+++ services/index.js
@@ -39,23 +39,98 @@ export const getPosts = async () => {
   return results.postsConnection.edges;
 };
 
+// export const getRecentPosts = async () => {
+//   const query = gql`
+//     query GetPostDetails() {
+//         posts(
+//             orderBy: createdAt_ASC
+//             last: 3
+//         ) {
+//             title
+//             featuredImage {
+//                 url
+//             }
+//             createdAt
+//             slug
+//         }
+//     }
+//     `;
+
+//   const result = await request(graphqlAPI, query);
+//   // console.log(result);
+//   return result.posts;
+// };
+
+// export const getSimilarPosts = async (categories, slug) => {
+//   // query language $slug --> type = string
+//   // where 果到講緊 donot display the current article but display other articles that includes some categories we want to get, and 我地想拎到last 3 article，所以寫做last 3
+//   const query = gql`
+//     query GetPostDetails($slug: String!, $categories: [String!]) {
+//       posts(
+//         where: {
+//           slug_not: $slug
+//           AND: { catrgories_some: { slug_in: $categories } }
+//         }
+//         last: 3
+//       ) {
+//         title
+//         featuredImage {
+//           url
+//         }
+//         createdAt
+//         slug
+//       }
+//     }
+//   `;
+
+//   const results = await request(graphqlAPI, query, { slug, categories });
+//   // console.log("This is " + results);
+//   return results.posts;
+// };
+
+// Answer
+
+export const getSimilarPosts = async (categories, slug) => {
+  const query = gql`
+    query GetPostDetails($slug: String!, $categories: [String!]) {
+      posts(
+        where: {
+          slug_not: $slug
+          AND: { categories_some: { slug_in: $categories } }
+        }
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query, { slug, categories });
+
+  return result.posts;
+};
+
 export const getRecentPosts = async () => {
   const query = gql`
-    query getPostDetails() {
-        posts(
-            orderBy: createAt_ASC
-            last: 3
-        ) {
-            title
-            featuredImage {
-                url
-            }
-            createdAt
-            slug
+    query GetPostDetails() {
+      posts(
+        orderBy: createdAt_ASC
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
         }
+        createdAt
+        slug
+      }
     }
-    `;
+  `;
+  const result = await request(graphqlAPI, query);
 
-  const results = await request(hraphqlAPI, query);
-  return results.postsConnection.edges;
+  return result.posts;
 };
